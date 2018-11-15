98cb660fdb36080788b86ff5f4828fd2ee270177
Dhawan Shringi <dhawanshringi@gmail.com>
2018-11-15 11:51:26 +0000

    Moving the utility method

---
+++ struts2-springsecurity4-tiles/src/main/java/com/demo/car/action/CommonUtil.java
@@ -0,0 +1,9 @@
+package com.demo.car.action;
+
+public class QuotingUtil {
+
+   public static String generateQuoteNo() {
+        // Is there a logic you have used behind this random generation or if that is also random?
+		return randomGeneratedQuoteNo = (int) (Math.random() * 9000) + 1000;
+	}
+}
